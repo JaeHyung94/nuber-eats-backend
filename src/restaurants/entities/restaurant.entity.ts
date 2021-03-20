d49b82cd43f0d3a472973d656c39773e9213b09f
JaeHyung94 <jhpark.main@gmail.com>
2021-03-20 17:05:43 +0900

    GraphQL and Postgres database configuration with ConfigModule, TypeORM and Joi

---
+++ src/restaurants/entities/restaurant.entity.ts
@@ -0,0 +1,14 @@
+import { Field, ObjectType } from "@nestjs/graphql";
+
+
+@ObjectType()
+export class Restaurant {
+    @Field(is => String)
+    name: string;
+    @Field(is => Boolean)
+    isVegan: boolean;
+    @Field(is => String)
+    address: string;
+    @Field(is => String)
+    ownerName: string;
+}
