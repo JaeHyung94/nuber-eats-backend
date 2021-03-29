cd2a1240de0d11e9e05f8f4da345e864e4717b79
JaeHyung94 <jhpark.main@gmail.com>
2021-03-29 17:59:29 +0900

    Unit Testing User Module 1

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -22,7 +22,6 @@ export class UsersService {
     @InjectRepository(User) private readonly users: Repository<User>,
     @InjectRepository(Verification)
     private readonly verification: Repository<Verification>,
-    private readonly config: ConfigService,
     private readonly jwtService: JwtService,
     private readonly mailService: MailService,
   ) {}
